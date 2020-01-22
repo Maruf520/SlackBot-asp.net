222bde6f763cc3c457e704e62b32b627d7baa940
Maruf <maruf02@student.sust.edu>
2020-01-22 20:53:30 +0600

    Setup SlackBot

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Windsor;
+using MargieBot;
+using System.ComponentModel;
+using System.Configuration;
+
+namespace Slackbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+            container.Register(Component.For<IResponder>().ImplementedBy<HelloResponder>());
+
+            var bot = new Bot();
+            var responders = container.ResolveAll<IResponder>();
+            foreach (var responder in responders)
+            {
+                bot.Responders.Add(responder);
+            }
+            var connect = bot.Connect(ConfigurationManager.AppSettings["SlackBotApiToken"]);
+
+            while (Console.ReadLine() != "close") { }
+        }
+    }
+}
